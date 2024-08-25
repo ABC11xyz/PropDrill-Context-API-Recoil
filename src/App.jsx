13a2b3b4462ce93c3777ca78b49e5de1d0d6cfcc
unknown <ruvishushukla1@gmail.com>
2024-08-25 15:50:51 +0530

    we do like this

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [count , setCount] = useState(0);
+
   return (
     <div>
-      Hii There
+      <Count count = {count}/>
+      <Buttons count = {count} setCount = {setCount} />
     </div>
   )
+}
+
+function Count ({count}) {
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons ({count , setCount}) {
+ return <div>
+  <button onClick={() => (setCount(count+1))}> Increse</button>
+  <button onClick={() => (setCount(count-1))}> Decrease</button>
+ </div>
 }
 
 export default App
